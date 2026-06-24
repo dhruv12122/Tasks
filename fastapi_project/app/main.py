a65157a03ffa837150a5f4150543e06c0fab0457
dhruv12122 <dhruvsable123@gmail.com>
2026-06-24 15:26:16 +0530

    created sqlite db and made user regestration

--- fastapi_project/app/main.py
+++ fastapi_project/app/main.py
@@ -1,7 +1,70 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
 
 app = FastAPI()
 
+conn = sqlite3.connect(
+    "smart_file_vault.db",
+    check_same_thread=False
+)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+)
+""")
+
+conn.commit()
+
+
 @app.get("/")
-def root():
-    return {"message": "Smart File Vault API"}  
+def home():
+    return {
+        "message": "Smart File Vault Running"
+    }
+
+
+class UserRegister(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+@app.post("/register")
+def register(user: UserRegister):
+
+    cursor.execute(
+        "SELECT * FROM users WHERE email = ?",
+        (user.email,)
+    )
+
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        return {
+            "message": "User already exists"
+        }
+
+    cursor.execute(
+        """
+        INSERT INTO users (name, email, password)
+        VALUES (?, ?, ?)
+        """,
+        (
+            user.name,
+            user.email,
+            user.password
+        )
+    )
+
+    conn.commit()
+
+    return {
+        "message": "User registered successfully"
+    }
