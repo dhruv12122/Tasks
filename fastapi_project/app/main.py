fa986c75e6834b445cec9f853b9594d04b187d71
dhruv12122 <dhruvsable123@gmail.com>
2026-06-24 20:16:13 +0530

    Implemented CRUD API with sqlite

--- fastapi_project/app/main.py
+++ fastapi_project/app/main.py
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS users(
 
 conn.commit()
 
-
+# Home page
 @app.get("/")
 def home():
     return {
@@ -35,7 +35,7 @@ class UserRegister(BaseModel):
     email: str
     password: str
 
-
+# For user regestration
 @app.post("/register")
 def register(user: UserRegister):
 
@@ -67,4 +67,91 @@ def register(user: UserRegister):
 
     return {
         "message": "User registered successfully"
+    }
+
+# To get all users
+@app.get("/users")
+def get_users():
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    return {
+        "id": users[0],
+        "name": users[1],
+        "email": users[2]
+    }
+
+# To get a single user
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    cursor.execute(
+        "SELECT * FROM users WHERE id = ?",
+        (user_id,)
+    )
+
+    user = cursor.fetchone()
+
+    if not user:
+        return {
+            "message": "User not found"
+        }
+
+    return {
+        "id": user[0],
+        "name": user[1],
+        "email": user[2]
+    }
+
+class UserUpdate(BaseModel):
+    name: str
+
+# For updating name 
+@app.put("/users/{user_id}")
+def update_name(user_id: int, user:UserUpdate):
+    cursor.execute(
+        "SELECT * FROM users WHERE id = ?",
+        (user_id,)
+    )
+    existing_user = cursor.fetchone()
+
+    if not existing_user:
+        return {
+            "message": "User not found"
+        }
+    
+    cursor.execute(
+        '''
+        UPDATE users SET name = ?
+        WHERE id = ? 
+        ''',
+        (user.name, user_id)
+    )
+    conn.commit()
+
+    return {
+        "message": "User updated successfully"
+    }
+
+# For deleting user
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    cursor.execute(
+        "SELECT * FROM users WHERE id = ?",
+        (user_id,)
+    )
+
+    current_user = cursor.fetchone()
+
+    if not current_user:
+        return {
+            "message": "User not found"
+        }
+
+    cursor.execute(
+        '''DELETE FROM users
+           WHERE id = ?''',
+            (user_id,)
+    )
+    conn.commit()
+    return {
+        "message": "User deleted successfully"
     }
